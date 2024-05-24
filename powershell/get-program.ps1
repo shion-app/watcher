9c924ae00cd5a81daa9b1336cd2d53fe383cecf6
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2024-05-24 14:52:22 +0800

    删除注释

--- powershell/get-program.ps1
+++ powershell/get-program.ps1
@@ -2,7 +2,6 @@ $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Obj
 
 Add-Type -AssemblyName System.Drawing
 
-# 创建空数组，用于存储数据
 $programs = @()
 
 function GetIconBytesByPath($path) {
@@ -17,28 +16,20 @@ function GetIconBytesByPath($path) {
     return $bytes
 }
 
-# 获取前台运行的应用列表
 $foregroundProcesses = Get-Process | Where-Object { $_.MainWindowHandle -ne 0 -and $_.MainWindowTitle -ne "" }
 
-# 输出应用列表信息
 foreach ($process in $foregroundProcesses) {
-    # $processId = $process.Id
     $processPath = $process.Path
 
-    # 检查是否为空值
     if (-not $processPath) {
-        # 如果 $processPath 为空值，跳过当前循环
         Continue
     }
 
-    # 使用 FileVersionInfo 获取更全面的信息
     $fileVersionInfo = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($processPath)
     $description = $fileVersionInfo.FileDescription
 
-    # 通过条件运算符创建 programName
     $name = if ($description) { $description } else { $process.ProcessName }
 
-    # 获取图标的缓冲区数据
     $iconBuffer = GetIconBytesByPath $processPath
 
     $program = @{
@@ -47,11 +38,8 @@ foreach ($process in $foregroundProcesses) {
         icon = $iconBuffer
     }
 
-    # 将数据添加到数组
     $programs += $program
 
-    # Write-Host "可执行文件路径: $processPath, name: $name, iconBuffer: $iconBuffer"
 }
 
-# 将数组转换为 JSON 格式并输出
 $programs | ConvertTo-Json
